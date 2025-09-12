8a08133def14f5e7409e73509b5cedf0137e6361
haserrano <hernan.serrano@jbb.gov.co>
2025-09-11 20:28:07 -0500

    Se completaron los comentarios faltantes

--- train.py
+++ train.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.linear_model import LinearRegression
+
+# Datos de entrada
 X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
 # y = 1 * x_0 + 2 * x_1 + 3
 y = np.dot(X, np.array([1, 2])) + 3

7251cbf9580b9be8f8b38bdff6e0225474ac37da
haserrano <hernan.serrano@jbb.gov.co>
2025-09-14 13:31:29 -0500

    Se agregaron cometarios

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
+# Importar librerías
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import cross_val_score
 from sklearn.tree import DecisionTreeRegressor
+
+# Crear dataset
 X, y = load_diabetes(return_X_y=True)
 regressor = DecisionTreeRegressor(random_state=0)
 cross_val_score(regressor, X, y, cv=10)

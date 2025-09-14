6ce9ed83192e7d24398897f6be3731d8a8471d5b
haserrano <hernan.serrano@jbb.gov.co>
2025-09-14 13:23:08 -0500

    Se creó código base para regresión con árboles

--- train.py
+++ train.py
@@ -1,12 +1,6 @@
-import numpy as np
-from sklearn.linear_model import LinearRegression
-
-# Datos de entrada
-X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-# y = 1 * x_0 + 2 * x_1 + 3
-y = np.dot(X, np.array([1, 2])) + 3
-reg = LinearRegression().fit(X, y)
-reg.score(X, y)
-reg.coef_
-reg.intercept_
-reg.predict(np.array([[3, 5]]))
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeRegressor
+X, y = load_diabetes(return_X_y=True)
+regressor = DecisionTreeRegressor(random_state=0)
+cross_val_score(regressor, X, y, cv=10)

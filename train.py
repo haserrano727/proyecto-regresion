f7a15d1dbbb906e40243c24d521a2063e496d66d
haserrano <hernan.serrano@jbb.gov.co>
2025-09-14 13:39:01 -0500

    Se eliminó comentario

--- train.py
+++ train.py
@@ -3,9 +3,13 @@ from sklearn.linear_model import LinearRegression
 
 # Datos de entrada
 X = np.array([[1, 1], [1, 2], [2, 2], [2, 3]])
-# y = 1 * x_0 + 2 * x_1 + 3
+
 y = np.dot(X, np.array([1, 2])) + 3
+
+# Creación y entrenamiento del modelo
 reg = LinearRegression().fit(X, y)
+
+# Validación del modelo entrenado
 reg.score(X, y)
 reg.coef_
 reg.intercept_
